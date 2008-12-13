7481d0b9ec0b9dff3f085a75c5ed722a2d3ce15c
ryanch <ryanch@fbe4cb8e-c94e-11dd-a412-a1792178251d>
2008-12-13 22:32:26 +0000

    added comments
    git-svn-id: https://bggiphone.googlecode.com/svn@8 fbe4cb8e-c94e-11dd-a412-a1792178251d

--- trunk/Classes/XmlSearchReader.h
+++ trunk/Classes/XmlSearchReader.h
@@ -25,15 +25,35 @@
 #import <UIKit/UIKit.h>
 @class BBGSearchResult;
 
+///
+/// This class is used to process the xml of a game search on boardgamegeek.com 
+///
 @interface XmlSearchReader : NSObject {
+	//! this stores the results, as BBGSearchResult obejcts 
 	NSMutableArray * searchResults;
+	
+	//! this is the number of games found 
 	int numberGamesFound;
+	
+	//! this is used when parsing, this is the current result that was found 
 	BBGSearchResult * currentResult;
+	
+	//! this is used when parsing, this holds the current text that is accumulated 
 	NSMutableString *stringBuffer;
+	
+	//! this is used when parsing,this is a list of game names found so far 
 	NSMutableArray *gameNames;
+	
+	//! this is used when parsing, true if the current game name being parsed is the primary name 
 	BOOL currentNameIsPrimary;
+	
+	//! this is used when parsing, tells us that we are in a name tag
 	BOOL inNameTag;
+	
+	//! this tells the parser what type of xml to expect. bgg has 2 types, one has <item> tags
 	BOOL parseItemFormat;
+	
+	//! this is the url that will be searched
 	NSURL *searchURL;
 }
 
@@ -48,9 +68,18 @@
 @property BOOL parseItemFormat;
 @property (nonatomic, retain ) NSURL * searchURL;
 
+///
+/// start the search at the url that is passed in
+/// overwritting the current value of searchURL
+/// when this is done, it will return YES if all worked
+/// if YES is returned then searchResults contains the results of the search.
+///
 - (BOOL)parseXMLAtURL:(NSURL *)URL parseError:(NSError **)error;
 
-
+///
+/// call parseXMLAtURL with the current value of the property
+/// searchURL
+/// 
 - (BOOL)parseXMLAtSearchURLWithError:(NSError **)error;
 
 
